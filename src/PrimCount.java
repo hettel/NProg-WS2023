e236819b58b8a210d7162dad4ce01d048942fe40
Joerg Hettel <joerg@MacBook-Pro-von-Jorg.local>
2023-10-04 11:05:33 +0200

    Examples 4.10.2023

---
+++ src/PrimCount.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.util.stream.LongStream;
+
+public class PrimCount
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Start serach with streams");
+        long start = System.currentTimeMillis();
+
+        long count =
+        LongStream.range(1_000_000L, 2_000_000L)
+                  .parallel()
+                  .mapToObj( BigInteger::valueOf )
+                  .filter( bi -> bi.isProbablePrime(1000))
+                  .count();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Elapsed time " + (end - start) + " [ms]");
+        System.out.println("Number of prims " + count);
+    }
+
+}
